c8086d10403bd2b561513a0cdde8ddb945236c12
whitewatercn <house_shores.5x@icloud.com>
2026-04-07 23:39:48 +0800

    重构代码结构

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
-import { gr } from './background/core.js';
-import { handleMessage } from './background/handlers.js';
+import { gr } from './src/background/core.js';
+import { handleMessage } from './src/background/handlers.js';
 
 chrome.runtime.onMessage.addListener(handleMessage);
 
